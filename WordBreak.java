af96f5eab63b1d692b53b97af871d99506474576
Kamal Singh <113432856+Kamal-Singh22@users.noreply.github.com>
2025-03-21 17:46:30 +0000

    Added the World Break Problem

---
+++ WordBreak.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class WordBreak {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        List<String> wordDict = Arrays.asList("leet", "code");
+        String s = "leetcode";
+        System.out.println("Can be segmented: " + wordBreak(s, wordDict)); // Output: true
+    }
+}
